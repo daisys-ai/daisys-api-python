e25ae82c3270328e0700adb606b6fd0aa67eda71
Stephen Sinclair <steve@daisys.ai>
2025-04-02 22:30:54 +0200

    Set version to 1.1.0.

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+import pathlib
+
 project = 'daisys-api'
-copyright = '2023, Daisys AI'
+copyright = '2025, Daisys AI'
 author = 'Daisys AI'
-release = '1.0.1'
+release = next(line.split('"')[1]
+               for line in open(pathlib.Path(__file__).parent.parent / 'pyproject.toml')
+               if line.startswith('version = '))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -42,6 +48,4 @@ html_theme_options = {
 }
 
 # -- Path setup --
-import os
-import sys
 sys.path.insert(0, os.path.abspath('..'))
